e1e406befda9c503bd5dc571fd6968ead450977a
Daniel Kim <kim.daniel24@gmail.com>
2019-10-19 18:16:30 -0400

    install axios

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,13 +1,15 @@
-import React from "react"
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-const Cart = () => (
+const Cart = () => {
+  
+  return(
   <Layout>
     <SEO title="Cart" />
     <h1>Cart</h1>
   </Layout>
-)
+  )
+}
 
 export default Cart
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,35 @@
-import React from "react"
+import React, { Component } from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import ProductList from "../components/product/product-list";
+import axios from 'axios';
+// Change this page to class component, then add modal and onClick event for 'Add Cart' button
+class IndexPage extends Component {
+  constructor(){
+    super();
+    
+    this.state = {
+      isModalOpen: false
+    }
+  }
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import ProductList from "../components/product/product-list"
+  componentDidMount() {
+    axios.get('data.json')
+    .then(res => {
+      console.log('retrieved data:', res.products)
+    })
+  }
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Products" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Products</h1>
-    <ProductList />
-  </Layout>
-)
+  
+  render() {
+    return (
+      <Layout>
+      <SEO title="Products" keywords={[`gatsby`, `application`, `react`]} />
+      <h1>Products</h1>
+      <ProductList />
+    </Layout>
+    )
+  }
+}
 
 export default IndexPage
