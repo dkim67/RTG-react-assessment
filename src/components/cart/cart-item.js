c0320942d5dc9d8ac0e404ae7eff37d28dfd0a40
Daniel Kim <kim.daniel24@gmail.com>
2019-10-20 20:41:30 -0400

    add increment and decrement (update) buttons

--- src/components/cart/cart-item.js
+++ src/components/cart/cart-item.js
@@ -1,11 +1,10 @@
 import React, { useContext, useState } from "react";
 import CartContext from '../../context/cart/cart.context';
 
-const CartItem = ({ product }) => {
+const CartItem = () => {
     const cart = useContext(CartContext);
 
-    const [ cartCollection, setNewCart] = useState(cart)
-    // console.log('this is the cart item props in cart', cartCollection);
+    const [ cartCollection, setNewCart] = useState(cart);
   
     return (
         (cartCollection.map(product => {
@@ -15,8 +14,16 @@ const CartItem = ({ product }) => {
                   <div className="product-image cell small-2"><img src={ product.image } alt={ product.title } /></div>
                   <div className="product-title cell small-4">{ product.title }</div>
                   <div className="product-sku cell small-2">{ product.sku }</div>
-                  <div className="product-price cell small-2">${ product.price }</div>
-                  <div className="product-add-to-cart cell small-2">
+                  <div className="product-price cell small-1">${ product.price }</div>
+                  <div className="product-price cell small-2">Quantity: { product.quantity } 
+                  <button value={product.sku} className="inc-dec-button" onClick={(e) => {
+                      let cartCheck = cartCollection.findIndex(item => item.sku === e.target.value)
+                      cartCollection[cartCheck].quantity += 1
+                      setNewCart(cartCollection)
+                      console.log("cart collection is increasing", cartCollection)
+                  }}>+</button>
+                  <button className="inc-dec-button">-</button></div>
+                  <div className="product-add-to-cart cell small-1">
                       <button value={product.sku} onClick={() => {
                           console.log(product.sku)
                           let filteredCartCollection = cartCollection.filter(p => {
